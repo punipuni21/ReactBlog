168ab487f8a8f3901a8bd1529509e00d4b3ea906
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-11-01 12:10:30 +0900

    make Portfolio dir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Page001 = () => <p>Page001</p>
-const Page002 = () => <p>Page002</p>
-const Page003 = () => <p>Page003</p>
-
-
 function App() {
 
   return (
---
+++ src/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import { Container, FormControl, Form, NavDropdown, Grid, Row, Col, Nav, Navbar, Button } from "react-bootstrap";
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Portfolio(){
+
+  
+
+
+
+
+
+}
