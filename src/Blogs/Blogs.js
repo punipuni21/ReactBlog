5ac900c50e1e66e7b6914a573ff4d8cfd7c603d0
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-11-01 13:50:40 +0900

    update .gitignore

--- src/Blogs/Blogs.js
+++ src/Blogs/Blogs.js
@@ -1,15 +1,19 @@
+import { React, Component } from 'react';
+import { InlineTex, Tex } from 'react-tex';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Blogs.css';
 
-
-function Blogs(){
-
-  return(
-
-    <p>ここにブログを書く</p>
-
-
-  );
+class Blogs extends Component{
+  render(){
+    let latexString = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
+    return(
+      <div>
+        <Tex texContent={latexString}/>
+        <br/>
+        <InlineTex texContent={latexString}/>
+      </div>
+    )
+  }
 }
 
 export default Blogs;
