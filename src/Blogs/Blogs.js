4f56f65330190b72b2dd23c2ad2b5d0c84c4641d
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-11-01 13:56:30 +0900

    update blog

--- src/Blogs/Blogs.js
+++ src/Blogs/Blogs.js
@@ -2,15 +2,21 @@ import { React, Component } from 'react';
 import { InlineTex, Tex } from 'react-tex';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Blogs.css';
+import 'katex/dist/katex.min.css'
+import { InlineMath, BlockMath } from 'react-katex';
 
 class Blogs extends Component{
   render(){
     let latexString = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
     return(
       <div>
-        <Tex texContent={latexString}/>
+        <Tex texContent="\int_{a}^{b} f(x)dx = F(b) - F(a)"/>
         <br/>
-        <InlineTex texContent={latexString}/>
+        <BlockMath math="\int_0^\infty x^2 dx"/>
+        <br/>
+        <InlineTex texContent="$$\int_{a}^{b} f(x)dx = F(b) - F(a)$$"/>
+        <br/>
+        <InlineMath>\int_0^\infty x^2 dx</InlineMath>
       </div>
     )
   }
